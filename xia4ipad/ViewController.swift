4edb3a2130e34bb27d11680b9810ff4530f49d45
Guillaume Barr√© <guillaume.barre@crdp.ac-versailles.fr>
2015-10-08 18:31:19 +0200

    Can add images from Camera.

--- xia4ipad/ViewController.swift
+++ xia4ipad/ViewController.swift
@@ -28,6 +28,22 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     var nbSVG:Int = 0
     
     @IBAction func btnCamera(sender: AnyObject) {
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
+            //load the camera interface
+            let picker : UIImagePickerController = UIImagePickerController()
+            picker.sourceType = UIImagePickerControllerSourceType.Camera
+            picker.delegate = self
+            picker.allowsEditing = false
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+        else{
+            //no camera available
+            let alert = UIAlertController(title: "Error", message: "There is no camera available", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .Default, handler: {(alertAction)in
+                alert.dismissViewControllerAnimated(true, completion: nil)
+            }))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
     }
     
     @IBAction func btnPhotoAlbum(sender: AnyObject) {
