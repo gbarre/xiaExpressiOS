gbarre/xiaExpressiOS

commit ac2ec00320648681ec008412174f138d3cbe91a7

Fix issue when create polygon & edit infos

xia/ViewCreateDetails.swift
@@ -698,6 +698,7 @@ class ViewCreateDetails: UIViewController, MFMailComposeViewControllerDelegate {
     func detailInfos() {
         moveDetail = false
         movingPoint = -1
+        stopCreation()
         if currentDetailTag == 0 {
             performSegue(withIdentifier: "viewMetas", sender: self)
         }

commit b349f8fa3855a937b005c2ef8ef6e8ff1b56e05f

Fix issue introduced by ac2ec003

xia/Info.plist
@@ -33,7 +33,7 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>1.2</string>
+	<string>1.3</string>
 	<key>CFBundleSignature</key>
 	<string>????</string>
 	<key>CFBundleVersion</key>

xia/Settings.bundle/Root.plist
@@ -14,7 +14,7 @@
 			<key>Key</key>
 			<string>version</string>
 			<key>DefaultValue</key>
-			<string>1.1</string>
+			<string>1.3</string>
 		</dict>
 		<dict>
 			<key>Type</key>

xia/ViewCreateDetails.swift
@@ -698,7 +698,9 @@ class ViewCreateDetails: UIViewController, MFMailComposeViewControllerDelegate {
     func detailInfos() {
         moveDetail = false
         movingPoint = -1
+        let tmpDetailTag = currentDetailTag
         stopCreation()
+        currentDetailTag = tmpDetailTag
         if currentDetailTag == 0 {
             performSegue(withIdentifier: "viewMetas", sender: self)
         }